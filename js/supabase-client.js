23b0e0962595d6af44c62718e069055a8901b516
AlanSosaR <alansosa225@gmail.com>
2025-11-14 11:13:54 +0000

    Create supabase-client.js

---
+++ js/supabase-client.js
@@ -0,0 +1,8 @@
+// supabase-client.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const SUPABASE_URL = "https://eaipcuvvddyrqkbmjmvw.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhaXBjdXZ2ZGR5cnFrYm1qbXZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwOTcxMDEsImV4cCI6MjA3ODY3MzEwMX0.2qICLx3qZgeGr0oXZ8PYRxXPL1X5Vog4UoOnTQBFzNA";
+
+// Cliente global de Supabase
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
