55a7a24654f906b5fdfde87c5705abaa2f1d7435
AlanSosaR <alansosa225@gmail.com>
2025-11-19 19:22:24 +0000

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,16 +1,20 @@
 // ===========================================
 // SUPABASE CLIENT — Café Cortero
-// Archivo para MÓDULOS ES6 (import/export)
+// MODO GLOBAL PARA NAVEGADOR
 // ===========================================
 
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+// Cargar createClient desde el SDK global
+const { createClient } = supabase;
 
 // Tu URL y ANON KEY
 const SUPABASE_URL = "https://eaipcuvvddyrqkbmjmvw.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhaXBjdXZ2ZGR5cnFrYm1qbXZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwOTcxMDEsImV4cCI6MjA3ODY3MzEwMX0.2qICLx3qZgeGr0oXZ8PYRxXPL1X5Vog4UoOnTQBFzNA";
 
-// Cliente Supabase listo para usar
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Crear cliente Supabase global
+const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-console.log("✅ Supabase conectado correctamente (modo ES modules)");
+// Guardarlo en el window
+window.supabaseClient = client;
+
+console.log("Supabase conectado correctamente (modo GLOBAL)");
