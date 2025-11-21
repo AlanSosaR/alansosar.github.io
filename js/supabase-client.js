c96caddf8d6d4cb8750170946c076eaa4abde866
AlanSosaR <alansosa225@gmail.com>
2025-11-21 15:24:04 +0000

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,17 +1,20 @@
 // ===========================================
 // SUPABASE CLIENT — Café Cortero
-// MODO GLOBAL
+// MODO GLOBAL (Fix temporal para GitHub Pages)
 // ===========================================
 
-// createClient VIENE del SDK cargado desde core-scripts.js
 const { createClient } = supabase;
 
-// URL y KEY
 const SUPABASE_URL = "https://eaipcuvvddyrqkbmjmvw.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhaXBjdXZ2ZGR5cnFrYm1qbXZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwOTcxMDEsImV4cCI6MjA3ODY3MzEwMX0.2qICLx3qZgeGr0oXZ8PYRxXPL1X5Vog4UoOnTQBFzNA";
 
-// Crear cliente global
-window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// 🔥 FIX IMPORTANTE PARA GITHUB PAGES
+// Usamos sessionStorage porque localStorage está bloqueado allí.
+window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: sessionStorage,
+  },
+});
 
-console.log("🔥 supabase-client.js cargado en modo GLOBAL");
+console.log("🔥 supabase-client.js cargado en modo GLOBAL (modo GitHub Pages)");
