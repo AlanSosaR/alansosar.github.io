9276909164cccdfffc0040e976c52f08b8f5cdc7
AlanSosaR <alansosa225@gmail.com>
2026-01-22 16:40:52 +0000

    Update firebase.js

--- js/core/firebase.js
+++ js/core/firebase.js
@@ -4,12 +4,12 @@ import { getMessaging } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 console.log("🔥 firebase.js — INIT ÚNICO");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABBa30g6cun08vQUC11e2AHuTPvoZLcI4",
+  apiKey: "AIzaSyA5Ba3Qs6cunO8vQUGi1e2AHuTPvozLcI4",
   authDomain: "cafecortero-eb674.firebaseapp.com",
   projectId: "cafecortero-eb674",
-  storageBucket: "cafecortero-eb674.appspot.com",
+  storageBucket: "cafecortero-eb674.firebasestorage.app",
   messagingSenderId: "412829554061",
-  appId: "1:412829554061:web:61a29b4f59881858f899c4"
+  appId: "1:412829554061:web:61a29b4f59881858f599c4"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
