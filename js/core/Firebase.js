16f825b69b3b6e4ee6124ebd063ef4522819bc2a
AlanSosaR <alansosa225@gmail.com>
2026-01-22 16:09:59 +0000

    Create Firebase.js

---
+++ js/core/Firebase.js
@@ -0,0 +1,17 @@
+// js/core/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js";
+
+console.log("🔥 firebase.js — INIT ÚNICO");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABBa30g6cun08vQUC11e2AHuTPvoZLcI4",
+  authDomain: "cafecortero-eb674.firebaseapp.com",
+  projectId: "cafecortero-eb674",
+  storageBucket: "cafecortero-eb674.appspot.com",
+  messagingSenderId: "412829554061",
+  appId: "1:412829554061:web:61a29b4f59881858f899c4"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const messaging = getMessaging(firebaseApp);
