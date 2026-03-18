11460b6f82755f86bbd33731c71a4f56307eee9b
Alan Sosa <alansosa225@gmail.com>
2026-03-18 12:43:06 +0000

    style: fix contact gap and position seal absolutely with larger size

--- js/pages/main.js
+++ js/pages/main.js
@@ -272,13 +272,15 @@ function renderMainProduct(product) {
         ${rightSpecs.filter(s => s.value).map(renderSpec).join("")}
       </div>
       <div class="spec-contact">
-        <img src="/imagenes/contact-mail.png" class="spec-icon-png" alt="Contacto">
-        <div class="spec-info">
-          <span class="spec-label">Contacto:</span>
-          <span class="spec-value">
-            <a href="https://wa.me/50496670613" target="_blank" class="contact-link">+504 9667-0613</a> / 
-            <a href="https://wa.me/50498675101" target="_blank" class="contact-link">9867-5101</a>
-          </span>
+        <div class="contact-info-wrap">
+          <img src="/imagenes/contact-mail.png" class="spec-icon-png" alt="Contacto">
+          <div class="spec-info">
+            <span class="spec-label">Contacto:</span>
+            <span class="spec-value">
+              <a href="https://wa.me/50496670613" target="_blank" class="contact-link">+504 9667-0613</a> / 
+              <a href="https://wa.me/50498675101" target="_blank" class="contact-link">9867-5101</a>
+            </span>
+          </div>
         </div>
         <img src="/imagenes/arabica.svg" class="arabica-seal" alt="100% Arábica">
       </div>
